2a1cd0016dda2abdcf2a905b8b3ee4ffd0b9350c
Guille <achoguille@gmail.com>
2026-06-26 01:26:53 -0300

    mejorando el estilo del login cambiando clases del css

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -2,40 +2,32 @@ import { useState } from 'react';
 import '../assets/static/styles/Login.css';
 
 const Login = () => {
-  // Estados para capturar lo que escribe el usuario
   const [nombre, setNombre] = useState('');
   const [sector, setSector] = useState('');
 
   const manejarEnvio = (e) => {
     e.preventDefault();
-    
-    // Por ahora mostramos en consola para verificar que funciona.
-    // El Integrante 2 va a usar estos datos para el AdminContext.
     console.log('Datos enviados:', { nombre, sector });
     
     alert(`¡Bienvenido ${nombre} del sector ${sector}!`);
   };
 
   return (
-    <div className="bg-login-gradient d-flex align-items-center justify-content-center vh-100 w-100 position-fixed top-0 start-0">
-      <div className="card login-card shadow-lg border-0 animate-fade-in">
-        <div className="card-body p-5">
-          
-          {/* Icono decorativo superior */}
+    <div className="contenedor-login">
+      <div className="card card-login border-0 shadow-lg" >
+        <div className="card-body bg-transparent p-5">
+          <h3 className="mt-3 text-center mb-4">  Panel  de Control</h3>
           <div className="text-center mb-4">
-            <div className="login-icon-box bg-primary text-white mx-auto shadow d-flex align-items-center justify-content-center">
-              <span className="fs-3">🔐</span>
+            <div className="login-icon-box mx-auto d-flex align-items-center justify-content-center">
+              <img src="https://img.icons8.com/?size=100&id=OAHfGI3jkWNU&format=png&color=000000" alt= "icn-login"/>
             </div>
-            <h2 className="fw-bold mt-3 mb-1 text-dark fs-3">Panel de Control</h2>
-            <p className="text-muted small">Ingresá tus credenciales de Administrador</p>
+            <p className="text-inicio fw-bold mb-4">Iniciar sesion</p>
           </div>
 
-          {/* Formulario de la consigna */}
           <form onSubmit={manejarEnvio}>
             
-            {/* Campo: Nombre */}
             <div className="mb-3 text-start">
-              <label htmlFor="nombre" className="form-label small fw-bold text-secondary">Nombre de Usuario</label>
+              <label htmlFor="nombre" className="form-label small fw-bold text-secondary"> Usuario</label>
               <input 
                 type="text" 
                 id="nombre" 
@@ -47,9 +39,8 @@ const Login = () => {
               />
             </div>
 
-            {/* Campo: Sector */}
             <div className="mb-4 text-start">
-              <label htmlFor="sector" className="form-label small fw-bold text-secondary">Sector / Rol</label>
+              <label htmlFor="sector" className="form-label small fw-bold text-secondary">Sector</label>
               <select 
                 id="sector" 
                 className="form-select form-control-lg fs-6" 
@@ -57,16 +48,14 @@ const Login = () => {
                 onChange={(e) => setSector(e.target.value)}
                 required
               >
-                <option value="" disabled>Seleccioná tu sector...</option>
+                <option value="" disabled> Seleciona sector...</option>
                 <option value="Gerencia">Gerencia</option>
                 <option value="Soporte">Soporte</option>
-                <option value="Administración">Administración</option>
               </select>
             </div>
 
-            {/* Botón Ingresar */}
             <button type="submit" className="btn btn-primary btn-lg w-100 fw-semibold shadow-sm py-2 fs-6">
-              Ingresar al Sistema
+              Ingresar 
             </button>
           </form>
 
